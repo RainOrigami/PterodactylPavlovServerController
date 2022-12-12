99d3ccef9e8e2c341721ee2675a4ac71bd7c2ca2
Longoon12000 <longoon12000@gmail.com>
2022-12-12 16:17:26 +0100

    Add confirmation dialog to player actions

---
+++ PterodactylPavlovServerController/Pages/Confirm.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PterodactylPavlovServerController.Pages;
+
+public partial class Confirm
+{
+    //protected bool ShowConfirmation { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public string Id { get; set; } = string.Empty;
+
+    [Parameter]
+    [EditorRequired]
+    public string ConfirmationTitle { get; set; } = string.Empty;
+
+    [Parameter]
+    [EditorRequired]
+    public string ConfirmationMessage { get; set; } = string.Empty;
+
+    //public void Show()
+    //{
+    //    ShowConfirmation = true;
+    //    StateHasChanged();
+    //}
+
+    [Parameter]
+    public EventCallback<bool> ConfirmationChanged { get; set; }
+
+    protected async Task OnConfirmationChange(bool value)
+    {
+        //ShowConfirmation = false;
+        await ConfirmationChanged.InvokeAsync(value);
+    }
+}
