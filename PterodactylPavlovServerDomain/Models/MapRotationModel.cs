35432b58a678ceb3cd5d07af7b7dc1512739cbdc
Longoon12000 <longoon12000@gmail.com>
2023-01-01 14:20:20 +0100

    Fixes empty map rotation

--- PterodactylPavlovServerDomain/Models/MapRotationModel.cs
+++ PterodactylPavlovServerDomain/Models/MapRotationModel.cs
@@ -9,5 +9,5 @@ public class MapRotationModel
     [MaxLength(64)]
     public string Name { get; set; }
     public ICollection<ServerMapModel> Maps { get; set; }
-    public List<MapInMapRotationModel> MapsInRotation { get; set; }
+    public List<MapInMapRotationModel>? MapsInRotation { get; set; }
 }
