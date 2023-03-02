901569a84415893a8701c64121741a5854992c06
Longoon12000 <longoon12000@gmail.com>
2023-03-02 17:49:53 +0100

    Removes debug console output

--- PterodactylPavlovServerController/Services/WarmupRoundService.cs
+++ PterodactylPavlovServerController/Services/WarmupRoundService.cs
@@ -31,32 +31,21 @@ public class WarmupRoundService
 
     private async void Connection_OnServerInfoUpdated(string serverId)
     {
-        if (connection.ServerId == "69b945c4")
-            Console.WriteLine($"{connection.ServerId} Connection_OnServerInfoUpdated");
         ServerSettings? warmupEnabled = await this.pavlovServerContext.Settings.FirstOrDefaultAsync(s => s.ServerId == connection.ServerId && s.SettingName == ServerSettings.SETTING_WARMUP_ENABLED);
         if (lastMap == null || lastRoundState == null || this.connection.ServerInfo!.GameMode != "SND" || warmupEnabled == null || !bool.TryParse(warmupEnabled.SettingValue, out bool isWarmupEnabled) || !isWarmupEnabled)
         {
-            if (connection.ServerId == "69b945c4")
-                Console.WriteLine($"{connection.ServerId} Not applicable");
             lastMap = this.connection.ServerInfo!.MapLabel;
             lastRoundState = this.connection.ServerInfo.RoundState;
             return;
         }
 
-        if (connection.ServerId == "69b945c4")
-            Console.WriteLine($"{connection.ServerId} Applicable");
-
         if (this.connection.ServerInfo!.MapLabel != lastMap)
         {
-            if (connection.ServerId == "69b945c4")
-                Console.WriteLine($"{connection.ServerId} Map just changed!");
             mapJustChanged = true;
         }
 
         if (lastRoundState == "Ended" && this.connection.ServerInfo!.RoundState == "StandBy" && mapJustChanged && !isWarmupRound && /* for safety */ this.connection.ServerInfo.Team1Score == 0 && this.connection.ServerInfo.Team0Score == 0)
         {
-            if (connection.ServerId == "69b945c4")
-                Console.WriteLine($"{connection.ServerId} Warmup round detected!");
             mapJustChanged = false;
             isWarmupRound = true;
 
@@ -64,8 +53,6 @@ public class WarmupRoundService
             if (warmupItems.Count > 0)
             {
                 Player[] players = await this.pavlovRconService.GetActivePlayers(apiKey, connection.ServerId);
-                if (connection.ServerId == "69b945c4")
-                    Console.WriteLine($"{connection.ServerId} Randomly giving {warmupItems.Count} different items to {players.Length} players");
                 foreach (Player player in players.Where(p => p.UniqueId.HasValue))
                 {
                     await this.pavlovRconService.GiveItem(apiKey, connection.ServerId, player.UniqueId!.Value, warmupItems[Random.Shared.Next(0, warmupItems.Count)].ToString());
@@ -75,14 +62,9 @@ public class WarmupRoundService
 
         if (lastRoundState == "Started" && this.connection.ServerInfo.RoundState == "Ended" && isWarmupRound)
         {
-            if (connection.ServerId == "69b945c4")
-                Console.WriteLine($"{connection.ServerId} Warmup round ending detected, resetting SND");
             isWarmupRound = false;
             await this.pavlovRconService.ResetSND(apiKey, connection.ServerId);
         }
-
-        if (connection.ServerId == "69b945c4")
-            Console.WriteLine($"{connection.ServerId} End.");
 
         lastMap = this.connection.ServerInfo!.MapLabel;
         lastRoundState = this.connection.ServerInfo.RoundState;
