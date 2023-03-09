f373de26def7debd4bc661515e80c57fdbfd424a
Longoon12000 <longoon12000@gmail.com>
2023-03-09 12:52:41 +0100

    Fixes pins not to be forced to 4 digits

--- PterodactylPavlovServerController/Models/SetPinModel.cs
+++ PterodactylPavlovServerController/Models/SetPinModel.cs
@@ -4,7 +4,7 @@ namespace PterodactylPavlovServerController.Models;
 
 public class SetPinModel
 {
-    [StringLength(4, MinimumLength = 4)]
+    [StringLength(4, MinimumLength = 1)]
     public string? Pin { get; set; }
 
     public bool? Set { get; set; }
